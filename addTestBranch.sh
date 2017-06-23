8e7f564dc418a458e372c13f8f67aa92cd397284
zscn <zhangshuai@gmail.com>
2017-06-23 14:10:54 -0400

    RepoA new commit at: 20170623-141052

---
+++ addTestBranch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NOW=`date "+%Y%m%d-%k%M%S"`
+BRANCH=RepoABranch_$NOW
+git fetch
+git rebase origin/master
+git co -b $BRANCH
+./addTestFile.sh
+printf "1. RepoA new commit at: $NOW\n" >> Readme.md 
+git add .
+git commit -m "RepoA new commit at: $NOW" 
+git push origin $BRANCH
+git co master
+git fetch
+git rebase origin/master
