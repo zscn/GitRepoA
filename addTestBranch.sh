e2c406b9606a3c273baf11b3fc6b80c73f7d633d
zscn <zhangshuai@gmail.com>
2017-06-23 14:14:07 -0400

    RepoA new commit at: 2017-06-23 14:14:05 EST

--- addTestBranch.sh
+++ addTestBranch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+TIMESTAMP=`date "+%Y-%m-%d %k:%M:%S %Z"`
 NOW=`date "+%Y%m%d-%k%M%S"`
 BRANCH=RepoABranch_$NOW
 git fetch
 git rebase origin/master
-git co -b $BRANCH
+git checkout -b $BRANCH
 ./addTestFile.sh
-printf "1. RepoA new commit at: $NOW\n" >> Readme.md 
+printf "1. RepoA new commit at: $TIMESTAMP\n" >> Readme.md 
 git add .
-git commit -m "RepoA new commit at: $NOW" 
+git commit -m "RepoA new commit at: $TIMESTAMP" 
 git push origin $BRANCH
-git co master
+git checkout master
 git fetch
 git rebase origin/master
